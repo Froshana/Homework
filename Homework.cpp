391f3319d2c5f9a122540ab4b9f715f45f7d9e50
Yevpraksiia Martyniuk <vinilclorid@gmail.com>
2025-06-12 13:19:45 +0300

    Перший коміт

---
+++ Homework.cpp
@@ -0,0 +1,48 @@
+// Завдання 1 //
+#include <iostream>
+using namespace std;
+int main(){
+    cout << "I love C++\n";
+    return 0;
+} 
+
+// Інший варіант//
+
+/* #include <iostream>
+int main ()
+{
+    std::cout << "I love C++" << std::endl;
+    return 0;
+}
+*/
+
+
+// Завдання 2 //
+
+#include <iostream>
+int main ()
+{
+return 0;
+}
+
+
+// Завдання 3//
+/*В цьому коді дві помилки
+Текст "Is there a bug here?" має буті в лапках, 
+тому що рядковий текст має бути в лапках у С++, 
+тоді компілятор буде розуміти, що це рядок символів, а не команда або інше.
+в кінці рядка має бути крапка з комою, бо це є завершення команди.*/
+
+//Завдання 4//
+
+#include <iostream>
+
+ using namespace std;
+ int main()
+{
+  cout << "Is there a bug here?";
+    return 0;
+}    
+  
+
+  //також можна додати << endl; в 42 рядок, для виведення переносу рядка.//

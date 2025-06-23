d460d75b34646ab3ad9ed33342186ff63366389e
Yevpraksiia Martyniuk <vinilclorid@gmail.com>
2025-06-23 15:17:51 +0300

    Домшнє завдання 5

--- Homework.cpp
+++ Homework.cpp
@@ -1,93 +1,100 @@
 #include <iostream>
-
 using namespace std;
 
- // Завдання 1 //
+// Завдання 1-2 //
+
+unsigned long int Perimeter (unsigned short int length, unsigned short int width)
+{
+   return 2 * (length + width); 
+}
 
 int main ()
-{ 
-   int myAge = 28;
-   int yourAge = 32;
-
-   if (myAge > yourAge)
-  {
-    myAge = yourAge;
-  }
-else
 {
-   yourAge = myAge;
-}
-cout << " My age = " << myAge << endl;
-cout << " Your age = " << yourAge << endl;
+   unsigned short int length, width;
 
-return 0;
-   
+   cout << "Enter length: ";
+   cin >> length;
+
+   cout << "Enter width: ";
+   cin >> width;
+
+   unsigned long int result = Perimeter (length,width);
+
+   cout << "Perimeter = " << result << endl;
+
+  return 0;
 } 
-
-// Завдання 2 //
-
-int main()
-{
-
-int a, b, c;
-cout << "Please enter three numbers) \n";
-
-cout << "a: ";
-cin >> a;
-
-cout << "\nb: ";
-cin >> b;
-
-cout << "\nc: ";
-cin >> c;
-
-if (c = (a - b)) 
-   cout << "\na: ";
-   cout << a;
-   cout << " minus b: ";
-   cout << b;
-   cout << " equals c: ";
-   cout << c << endl;
-
-}
-
-else
-
-   cout << "a - b does not equal c: " << endl;
-   
-return 0;
-} 
-
-/* Тут присвоєння замість порівняння, с присвоює результат а - b
-тому умова завжди буде true, якщо a-b не дорівнює 0.  Яке б ціле число ми не ввели для с, результат буде перезаписан та нам буде видаваться результат віднімання а та b.
-при a - b = 0, компілятор буде виводити результат, який вказан в умові else. */
-
 
 // Завдання 3 //
 
-/* Якщо запустити попередню програму та ввести значення a=20, b=10, c=50,
-то нам видасть результат  a: 20 minus b: 10 equals c: 10, тому що програма проігнорувала 
-умову else та перезаписала значення с, і порахувала як 20-10=10, тож с = 10. */
+#include <iostream>
+using namespace std;
+
+void myFunc(unsigned short int x); // функція void не повертає значення, краще замінити void на unsigned short int //
+int main()
+{
+    unsigned short int x, y;
+    y = myFunc(int); // неправильний виклик функціі,  ми передаємо змінну, а не тип, правильно буде так y = myFunc(x) //
+    cout << "x: " << x << "y: " << y << "\n"; // x не була ініціалізована, можна або присвоїти їй число, або задопомоги cin взяти число від користувача //
+// немає return 0; яка повідомляє операційній системі, що програма завершилася успішно //
+}
+void myFunc(unsigned short int x) //  функція myFunc оголошена, як void, як я писала треба замінити на unsigned short int umyFunc(unsigned short int x), або int //
+{
+    return (4 * x);
+}
+
 
 // Завдання 4 //
 
+#include <iostream>
+using namespace std;
+
+int myFunc(unsigned short int x); 
+
 int main()
-
 {
-   int a = 1, b = 1, c;
+    unsigned short int x, y;
+    y = myFunc(x);
+    cout << "x: " << x << " y: " << y << "\n"; // неініціалізована х, або треба оголосити або отримати через cin //
+    // немає return 0;
+}
 
-	if (c = (a - b)) // можна додати дужки і копілято не буде видавати попередження: ((c = (a - b)))
-  
+int myFunc(unsigned short int x); // функція неправильно реалізована, крапка з комою закривє функцію //
+{
+    return (4 * x); // не обовязково тут ставити дужки, але можна і з ними //
+}
 
-    	cout << "The value of c is: " << c;
 
-	return 0;
+// Завдання 5-6 //
+
+int piypiy (unsigned short int x, unsigned short int y)
+{
+if (y != 0) 
+   return x / y;
+
+else
+   return -1; 
+}
+
+int main ()
+{
+   unsigned short int x,y;
+
+   cout << "Enter first number: ";
+   cin >> x;
+
+   cout << "Enter second number: ";
+   cin >> y;
+   
+   int result = piypiy(x,y);
+
+if (result == -1)
+   cout << "Error! " << endl;
+
+else 
+   cout << "Result is: " << result << endl;
+
+// скобки в умовах if та else я не забула, а проігнорувала, бо в мене є тільки один оператор cout, то їх можна не ставити, бо немає чого групурувати //
+return 0;
 
 }
-/* Тож с не ініціализоване в 77 строці, але в if присвоюється значення a - b. Відбувається присвоєння с = 0 і умова if не виконується бо с=0 false, в нас немає else
-тож програма просто завершується.
-*/
-
-// Завдання 5 //
- /* Програма видала попередження з приводу присвоєння с, тому що тут присвоєння =, а не порівняння == і не додані ще одні дужки ()
-Ще в результаті було нічого так як: в нас є присвоєння с, с завжди буде = 0, це буде завжди false, програма завершується бо в нас немає else. */

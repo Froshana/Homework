4b1695b68d8b5ce6b902c42c4fd5a121f5c4e6c5
Yevpraksiia Martyniuk <vinilclorid@gmail.com>
2025-07-18 14:40:38 +0300

    Homework 12

--- Homework.cpp
+++ Homework.cpp
@@ -3,56 +3,112 @@ using namespace std;
 
 // Завдання 1 //
 
-class Rectangle {
- public:
-	virtual ~Rectangle(){}
+const int mapHEIGHT = 3;
+const int mapWDTH = 3;
 
-	virtual Rectangle* clone() const = 0;
-
-	virtual void draw() const {
-		cout << "Rectangle" << endl;
-	}
+char mapBoard [mapHEIGHT][mapWDTH]= {
+	{' ',' ',' '},
+	{' ',' ',' '},
+	{' ',' ',' '}
 };
 
-class Square : public Rectangle {
- public:
-	Square() = default;
-	Square(const Square& other) = default;
-
-	virtual Square* clone() const override {
-		return new Square(*this);
+void printBoard(){
+	for(int i = 0; i < mapHEIGHT; i++){
+		cout << " ";
+		for (int j = 0; j < mapWDTH; j++){
+			cout << mapBoard[i][j];
+			if (j < mapWDTH - 1)
+			cout << " | ";
+		}
+		cout << endl;
+		if (i < mapWDTH - 1)
+		cout << "---+---+--- " << endl;
 	}
-	void draw() const override {
-		cout << "Square" << endl;
-	}
-};
-
-void printCopy(const Rectangle* r){
-	Rectangle* copy = r->clone();
-	copy->draw();
-	delete copy;
 }
 
-int main (){
-	Square sq;
-	printCopy(&sq);
+void playerMove(char symbol){
+	int row, col;
+	while (true){
+		cout << "Гравець " << symbol << ", ведіть рядок (1-3) та стовпець (1-3) : ";
+		cin >> row >> col;
+		row--;
+		col--;
+
+		if (row >= 0 && row < mapHEIGHT &&
+			col >= 0 && col < mapWDTH &&
+			mapBoard[row][col] == ' '){
+
+				mapBoard [row][col] = symbol;
+				break;
+			} else{
+				cout << "Будь ласка, введіть ще раз! ";
+		}
+	}
+	
+}
+
+ int main (){
+	printBoard();
+
+	playerMove('X');
+	printBoard();
+
+	playerMove('O');
+	printBoard();
+
+	playerMove('X');
+	printBoard();
+
+	playerMove('X');
+	printBoard();
+
+	playerMove('X');
+	printBoard();
+
+
+	return 0;
+ }
+// Я просто по фану вирішила написати гру
+
+ // Завдання 2 //
+
+#include <iostream>
+using namespace std;
+
+int main() {
+ const int HEIGHT = 3;
+ const int WDTH = 3;
+
+ 	int board [HEIGHT][WDTH];
+
+	for	(int i = 0; i < HEIGHT; i++){
+		for (int j = 0; j < WDTH; j++){
+		board[i][j] = 0;
+		}
+	}
+
+	for(int i = 0; i < HEIGHT; i++){
+		for (int j = 0; j < WDTH; j++){
+		cout << board[i][j] << " ";
+		}
+		cout << endl;
+	}
 
 	return 0;
 }
 
-// Завдання 2 //
-
-void SomeFunction(Shape); // функція приймає параметр за значенням
-Shape *pRect = new Rectangle; // *pRect об'єкт типу Rectangle але копіюється лише  базова частина, усі додаткові поля та методи Rectangle зникають у копії
-SomeFunction(*pRect);
-
-
 // Завдання 3 //
 
-class Shape
-{
-public:
-	Shape();
-	virtual ~Shape();
-	virtual Shape(const Shape&); // конструктор не може бути віртуальнимБ немає реалізації конструктора/деструктора
-};
+	unsigned short SomeArray[5][4];
+	for (int i = 0; i < 4; i++) // тут переплутані стовпці та рядки, спочатку рядки (5) потім стовпці (4), тут один рядок залишається неініціалізованим
+    		for (int j = 0; j < 5; j++) // це вихід за межі стовпця, тут повинно бути цифра 4, або можна поміняти місцями цифри
+        			SomeArray[i][j] = i + j; //
+
+
+
+// Завдання 4 //
+
+	unsigned short SomeArray[5][4];
+	for (int i = 0; i <= 5; i++) // == 5 це вихід на межі масиву, може бути <= 4, бо нумерація починається з 0
+    		for (int j = 0; j <= 4; j++) // тут так само, може бути <=3
+	        	SomeArray[i][j] = 0;

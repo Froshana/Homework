eaef18ee6001233b495aaa4c32028f440ef178d2
Yevpraksiia Martyniuk <vinilclorid@gmail.com>
2025-07-24 17:30:09 +0300

    Homework 13

--- Homework.cpp
+++ Homework.cpp
@@ -1,114 +1,54 @@
+ // Завдання 1 //
+
+Теоретично максимальна кількість елементів у зв'язному спіску може бути необмежаною, бо немає фіксованого розміру
+На практці кількість обмежується оперативною пам'яттю, розрядність систем (34бітна програма обмежана десь 4 гб памяті, 64бітна - терабайтом)
+Типом вузла - чим більше вузол має данних, тим більше він займає пам'яті
+Операційною системою - видає помилку, якщо не вистачає пам'яті
+
+  // Завдання 2 //
+
+У зв'язному списку можна використовувати індекси,але це не дуже має сенс через неефективність
+Бо буде повільний доступ, вузли будуть розкидані по пам'яті, з показжчиками, не має прямого доступу
+Щоб отримати елемент за індексом у зв'язаному списку потрібно буде рухатися по списку вручну
+Зв'язний список насправді не має справжніх індексов - це все рахується вручну
+
+   // Завдання 3 //
+
+ Останній символ у рядку "Сергій - хороший хлопець" - це ь
+ Перевіремо */
 #include <iostream>
+#include <string>
 using namespace std;
 
-// Завдання 1 //
+int main(){
+	string text = "Сергій - хороший хлопець";
+	char lastChar = text[text.length() - 1];
+	cout << "Останній символ : " << lastChar << endl;
+}
 
-const int mapHEIGHT = 3;
-const int mapWDTH = 3;
+    // Завдання 4 //
 
-char mapBoard [mapHEIGHT][mapWDTH]= {
-	{' ',' ',' '},
-	{' ',' ',' '},
-	{' ',' ',' '}
+class Node{
+	public:
+		int data;
+		Node* next;
+
+		Node(int value){
+			data = value;
+			next = nullptr;
+		}
 };
 
-void printBoard(){
-	for(int i = 0; i < mapHEIGHT; i++){
-		cout << " ";
-		for (int j = 0; j < mapWDTH; j++){
-			cout << mapBoard[i][j];
-			if (j < mapWDTH - 1)
-			cout << " | ";
-		}
-		cout << endl;
-		if (i < mapWDTH - 1)
-		cout << "---+---+--- " << endl;
-	}
-}
+int main(){
+	Node* first = new Node(10);
+	Node* second = new Node(20);
+	first -> next = second;
 
-void playerMove(char symbol){
-	int row, col;
-	while (true){
-		cout << "Гравець " << symbol << ", ведіть рядок (1-3) та стовпець (1-3) : ";
-		cin >> row >> col;
-		row--;
-		col--;
+	cout << "" << first->data << endl;
+	cout << "" << first->next->data << endl;
 
-		if (row >= 0 && row < mapHEIGHT &&
-			col >= 0 && col < mapWDTH &&
-			mapBoard[row][col] == ' '){
-
-				mapBoard [row][col] = symbol;
-				break;
-			} else{
-				cout << "Будь ласка, введіть ще раз! ";
-		}
-	}
-	
-}
-
- int main (){
-	printBoard();
-
-	playerMove('X');
-	printBoard();
-
-	playerMove('O');
-	printBoard();
-
-	playerMove('X');
-	printBoard();
-
-	playerMove('X');
-	printBoard();
-
-	playerMove('X');
-	printBoard();
-
-
-	return 0;
- }
-// Я просто по фану вирішила написати гру
-
- // Завдання 2 //
-
-#include <iostream>
-using namespace std;
-
-int main() {
- const int HEIGHT = 3;
- const int WDTH = 3;
-
- 	int board [HEIGHT][WDTH];
-
-	for	(int i = 0; i < HEIGHT; i++){
-		for (int j = 0; j < WDTH; j++){
-		board[i][j] = 0;
-		}
-	}
-
-	for(int i = 0; i < HEIGHT; i++){
-		for (int j = 0; j < WDTH; j++){
-		cout << board[i][j] << " ";
-		}
-		cout << endl;
-	}
+	delete first;
+	delete second;
 
 	return 0;
 }
-
-// Завдання 3 //
-
-	unsigned short SomeArray[5][4];
-	for (int i = 0; i < 4; i++) // тут переплутані стовпці та рядки, спочатку рядки (5) потім стовпці (4), тут один рядок залишається неініціалізованим
-    		for (int j = 0; j < 5; j++) // це вихід за межі стовпця, тут повинно бути цифра 4, або можна поміняти місцями цифри
-        			SomeArray[i][j] = i + j; //
-
-
-
-// Завдання 4 //
-
-	unsigned short SomeArray[5][4];
-	for (int i = 0; i <= 5; i++) // == 5 це вихід на межі масиву, може бути <= 4, бо нумерація починається з 0
-    		for (int j = 0; j <= 4; j++) // тут так само, може бути <=3
-	        	SomeArray[i][j] = 0;
